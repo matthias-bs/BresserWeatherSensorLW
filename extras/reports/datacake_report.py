71aa1f810fce68e0e9901d0041b297639bf36c34
Matthias Prinke <m.prinke@arcor.de>
2024-08-30 18:49:28 +0200

    Moved code into functions

--- extras/reports/datacake_report.py
+++ extras/reports/datacake_report.py
@@ -5,18 +5,19 @@ import matplotlib.dates as mdates
 from matplotlib.backends.backend_pdf import PdfPages
 
 # Directory containing the CSV files
-dir = "/home/mp/pCloudDrive/datacake/Garten"
+src_dir = "/home/mp/pCloudDrive/datacake/Garten"
+pdf_file = "/home/mp/pCloudDrive/datacake/Garten/garten.pdf"
 last_column = 16
 
 # List all CSV files in the directory
-csv_files = [f for f in os.listdir(dir) if f.endswith('.csv')]
+csv_files = [f for f in os.listdir(src_dir) if f.endswith('.csv')]
 
 # Initialize an empty list to store DataFrames
 dataframes = []
 
 # Read each CSV file
 for file in csv_files:
-    file_path = os.path.join(dir, file)
+    file_path = os.path.join(src_dir, file)
     df = pd.read_csv(file_path, skiprows=0, parse_dates=[0], date_parser=lambda x: pd.to_datetime(x, format='%a, %d %b %Y %H:%M:%S'))
     
     for col in range(1, last_column):
@@ -43,44 +44,68 @@ combined_df.dropna(inplace=True)
 # Drop rows where column 3 (Humidity) is zero
 combined_df = combined_df[combined_df.iloc[:, 1] != 0]
 
-# Create a PdfPages object to save the figures
-with PdfPages('garten.pdf') as pdf:
-    # Create a title page
+
+def title_page(plt, pdf, title, font_size=24):
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax.text(0.5, 0.5, 'Wetterbericht Lehmanger, Garten 95', transform=ax.transAxes, fontsize=24, ha='center', va='center')
+    ax.text(0.5, 0.5, title, transform=ax.transAxes, fontsize=font_size, ha='center', va='center')
     ax.axis('off')
     pdf.savefig(fig)
     plt.close(fig)
 
-    # Create a figure with two subplots
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+def plot_data(plt, pdf, df, columns, title, xlabel, ylabels, colors, avg_label, avg_colors):
 
-    # Plot column 0 on the first subplot
-    ax1.plot(combined_df.index, combined_df.iloc[:, 0], label='Temperatur [°C]', color='r')
-    ax1.set_xlabel('Zeit')
-    ax1.set_ylabel('Temperatur [°C]', color='r')
-    ax1.tick_params(axis='y', labelcolor='r')
+    fig, axes = plt.subplots(len(columns), 1, figsize=(12, 6))
+    for i in range(len(columns)):
+        if len(columns) == 1:
+            ax = axes
+        else:
+            ax = axes[i]
+        ax.plot(df.index, df.iloc[:, columns[i]], label=ylabels[i], color=colors[i])
+        if xlabel:
+            ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabels[i])
+        #ax.tick_params(axis='y', labelcolor=colors[i])
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
+        ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+        if avg_colors[i]:
+            daily_avg = df.iloc[:, columns[i]].resample('D').mean()
+            ax.plot(daily_avg.index, daily_avg, label=f'{avg_label} {ylabels[i]}', color=avg_colors[i], linestyle='--', linewidth=2)
+
+        ax.legend()
+        ax.grid(True)
+        
+        # Rotate x-axis labels for better readability
+        plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+
+    fig.suptitle(title, fontsize=16)
+    plt.tight_layout()
+
+    pdf.savefig(fig)
+    plt.close(fig)
+
+# Create a new figure for Rain Gauge
+def plot_rain(plt, pdf, df_diff, df, title, xlabel, ylabel, color):
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6))
+
+    # Plot the Rain Gauge differences as a bar graph
+    ax1.bar(df_diff.index, df_diff, label=ylabel[0], color=color)
+    if xlabel:
+        ax1.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel[0])
+    #ax.tick_params(axis='y', labelcolor='b')
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
     ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
     
-    # Calculate and plot daily average for temperature
-    daily_avg_temperature = combined_df.iloc[:, 0].resample('D').mean()
-    ax1.plot(daily_avg_temperature.index, daily_avg_temperature, label='Täglicher Durchschnitt [°C]', color='k', linestyle='--', linewidth=2)
-
     ax1.legend()
     ax1.grid(True)
 
     # Plot column 1 on the second subplot
-    ax2.plot(combined_df.index, combined_df.iloc[:, 1], label='rel. Feuchte [%]', color='b')
-    ax2.set_xlabel('Zeit')
-    ax2.set_ylabel('rel. Feuchte [%]', color='b')
-    ax2.tick_params(axis='y', labelcolor='b')
+    ax2.plot(df.index, df.iloc[:, 6], label=ylabel[1], color=color)
+    if xlabel:
+        ax2.set_xlabel(xlabel)
+    ax2.set_ylabel(ylabel[1])
     ax2.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
     ax2.xaxis.set_major_locator(mdates.AutoDateLocator())
-
-    # Calculate and plot daily average for humidity
-    daily_avg_humidity = combined_df.iloc[:, 1].resample('D').mean()
-    ax2.plot(daily_avg_humidity.index, daily_avg_humidity, label='Täglicher Durchschnitt [%]', color='k', linestyle='--', linewidth=2)
 
     ax2.legend()
     ax2.grid(True)
@@ -92,192 +117,53 @@ with PdfPages('garten.pdf') as pdf:
     # Adjust layout to prevent clipping of tick-labels
     plt.tight_layout()
 
-    # Show the plot
-    #plt.show()
+    # Set the title of the figure
+    fig.suptitle(title, fontsize=16)
 
     # Save the current figure to the PDF
     pdf.savefig(fig)
-    
+
     # Close the figure to free up memory
     plt.close(fig)
+                 
+# Create a PdfPages object to save the figures
+with PdfPages(pdf_file) as pdf:
+    title_page(plt, pdf, 'Wetterbericht Lehmanger, Garten 95')
+
+    title_page(plt, pdf, 'Jahresübersicht', 18)
+
+    plot_data(plt, pdf, combined_df, [0, 1], 'Luftemperatur und -feuchte', None, ['Temperatur [°C]', 'rel. Feuchte [%]'],['r', 'b'], 'tägl. Durchschnitt', ['k', 'k'])
+
+    # Filter out rows where soil moisture (column 9) is zero
+    soil_df = combined_df[combined_df.iloc[:, 9] != 0]
+    plot_data(plt, pdf, soil_df, [10, 9], 'Bodentemperatur und -feuchte', None, ['Temperatur [°C]', 'Feuchte [%]'],['m', 'g'], 'tägl. Durchschnitt', ['k', None])
+
+    # Calculate the difference between consecutive values in column 6 (Rain Gauge)
+    rain_df = combined_df.iloc[:, 6].diff().fillna(0)
+    rain_df = rain_df[rain_df >= 0]
+    plot_rain(plt, pdf, rain_df, combined_df, 'Niederschlag', None, ['Niederschlag [mm]', 'Niederschlag [mm] (Regenmesser)'], 'b')
+
+    plot_data(plt, pdf, combined_df, [11], 'Warmwasser', None, ['Temperatur [°C]'], ['orange'], 'tägl. Durchschnitt', ['k'])
+
+    title_page(plt, pdf, 'Monatsberichte', 18)
 
     # Group the data by month
     combined_df['Month'] = combined_df.index.to_period('M')
 
     # Create a separate figure for each month
     for month, month_df in combined_df.groupby('Month'):
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+        title_page(plt, pdf, month, 16)
 
-        # Plot column 0 on the first subplot
-        #ax1.scatter(month_df.index, month_df.iloc[:, 0], label='Temperatur [°C]', color='r', s=10, marker='+')
-        ax1.plot(month_df.index, month_df.iloc[:, 0], label='Temperatur [°C]', color='r')
-        ax1.set_xlabel('Zeit')
-        ax1.set_ylabel('Temperatur [°C]', color='r')
-        ax1.tick_params(axis='y', labelcolor='r')
-        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
-
-        # Calculate and plot daily average for temperature
-        daily_avg_temperature = month_df.iloc[:, 0].resample('D').mean()
-        ax1.plot(daily_avg_temperature.index, daily_avg_temperature, label='Täglicher Durchschnitt [°C]', color='r', linestyle='--', linewidth=2)
-
-        ax1.legend()
-        ax1.grid(True)
-
-        # Plot column 1 on the second subplot
-        #ax2.scatter(month_df.index, month_df.iloc[:, 1], label='rel. Feuchte [%]', color='b', s=10, marker='+')
-        ax2.plot(month_df.index, month_df.iloc[:, 1], label='rel. Feuchte [%]', color='b')
-        ax2.set_xlabel('Zeit')
-        ax2.set_ylabel('rel. Feuchte [%]', color='b')
-        ax2.tick_params(axis='y', labelcolor='b')
-        ax2.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax2.xaxis.set_major_locator(mdates.AutoDateLocator())
-        
-
-        # Calculate and plot daily average for humidity
-        daily_avg_humidity = month_df.iloc[:, 1].resample('D').mean()
-        ax2.plot(daily_avg_humidity.index, daily_avg_humidity, label='Täglicher Durchschnitt [%]', color='b', linestyle='--', linewidth=2)
-
-        ax2.legend()
-        ax2.grid(True)
-
-        # Rotate x-axis labels for better readability
-        plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
-        plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
-
-        # Adjust layout to prevent clipping of tick-labels
-        plt.tight_layout()
-
-        # Set the title of the figure
-        fig.suptitle(f'Garten {month} - Luft', fontsize=16)
-
-        # Show the plot
-        #plt.show()
-
-        # Save the current figure to the PDF
-        pdf.savefig(fig)
-        
-        # Close the figure to free up memory
-        plt.close(fig)
+        plot_data(plt, pdf, month_df, [0, 1], f'Luftemperatur und -feuchte {month}', None, ['Temperatur [°C]', 'rel. Feuchte [%]'],['r', 'b'], 'tägl. Durchschnitt', ['k', 'k'])
 
         # Filter out rows where soil moisture (column 9) is zero
-        month_soil_df = month_df[month_df.iloc[:, 9] != 0]
-
-        # Create a new figure for Soil Temperature and Soil Moisture
-        fig, (ax3, ax4) = plt.subplots(2, 1, figsize=(12, 12))
-        
-        # Plot column 10 (Soil Temperature) on the first subplot as small dots
-        ax3.plot(month_soil_df.index, month_soil_df.iloc[:, 10], label='Bodentemperatur [°C]', color='g')
-        ax3.set_xlabel('Zeit')
-        ax3.set_ylabel('Bodentemperatur [°C]', color='g')
-        ax3.tick_params(axis='y', labelcolor='g')
-        ax3.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax3.xaxis.set_major_locator(mdates.AutoDateLocator())
-        
-        # Calculate and plot daily average for Soil Temperature
-        daily_avg_soil_temp = month_soil_df.iloc[:, 10].resample('D').mean()
-        avg_soil_temp = daily_avg_soil_temp.mean()
-        ax3.plot(daily_avg_soil_temp.index, daily_avg_soil_temp, label='Täglicher Durchschnitt [°C]', color='g', linestyle='--', linewidth=2)
-        
-        ax3.legend()
-        ax3.grid(True)
-
-        # Plot column 9 (Soil Moisture) on the second subplot as small dots
-        ax4.plot(month_soil_df.index, month_soil_df.iloc[:, 9], label='Bodenfeuchte [%]', color='m')
-        ax4.set_xlabel('Zeit')
-        ax4.set_ylabel('Bodenfeuchte [%]', color='m')
-        ax4.tick_params(axis='y', labelcolor='m')
-        ax4.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax4.xaxis.set_major_locator(mdates.AutoDateLocator())
-        
-        # Calculate and plot daily average for Soil Moisture
-        daily_avg_soil_moisture = month_soil_df.iloc[:, 9].resample('D').mean()
-        avg_soil_moisture = daily_avg_soil_moisture.mean()
-        ax4.plot(daily_avg_soil_moisture.index, daily_avg_soil_moisture, label='Täglicher Durchschnitt [%]', color='m', linestyle='--', linewidth=2)
-        
-        ax4.legend()
-        ax4.grid(True)
-
-        # Rotate x-axis labels for better readability
-        plt.setp(ax3.xaxis.get_majorticklabels(), rotation=45)
-        plt.setp(ax4.xaxis.get_majorticklabels(), rotation=45)
-        
-        # Adjust layout to prevent clipping of tick-labels
-        plt.tight_layout()
-
-        # Set the title of the figure
-        fig.suptitle(f'Garten {month} - Boden', fontsize=16)
-        
-        # Save the current figure to the PDF
-        pdf.savefig(fig)
-        
-        # Close the figure to free up memory
-        plt.close(fig)
-
-        # Create a new figure for Water Temperature
-        fig, ax5 = plt.subplots(1, 1, figsize=(12, 12))
-
-        # Plot column 11 (Water Temperature)
-        ax5.plot(month_df.index, month_df.iloc[:, 11], label='Wassertemperatur [°C]', color='orange')
-        ax5.set_xlabel('Zeit')
-        ax5.set_ylabel('Wassertemperatur [°C]', color='orange')
-        ax5.tick_params(axis='y', labelcolor='orange')
-        ax5.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax5.xaxis.set_major_locator(mdates.AutoDateLocator())
-        
-        # Calculate and plot daily average for Water Temperature
-        daily_avg_water_temp = month_df.iloc[:, 11].resample('D').mean()
-        ax5.plot(daily_avg_water_temp.index, daily_avg_water_temp, label='Täglicher Durchschnitt [°C]', color='orange', linestyle='--', linewidth=2)
-        
-        ax5.legend()
-        ax5.grid(True)
-
-        # Rotate x-axis labels for better readability
-        plt.setp(ax5.xaxis.get_majorticklabels(), rotation=45)
-
-        
-        # Adjust layout to prevent clipping of tick-labels
-        plt.tight_layout()
-
-        # Set the title of the figure
-        fig.suptitle(f'Garten {month} - Warmwasser', fontsize=16)
-        
-        # Save the current figure to the PDF
-        pdf.savefig(fig)
-        
-        # Close the figure to free up memory
-        plt.close(fig)
-
-        # Create a new figure for Rain Gauge
-        fig, ax6 = plt.subplots(1, 1, figsize=(12, 6))
+        soil_df = month_df[month_df.iloc[:, 9] != 0]
+        plot_data(plt, pdf, soil_df, [10, 9], f'Bodentemperatur und -feuchte {month}', None, ['Temperatur [°C]', 'Feuchte [%]'],['m', 'g'], 'tägl. Durchschnitt', ['k', None])
 
         # Calculate the difference between consecutive values in column 6 (Rain Gauge)
-        rain_diff = month_df.iloc[:, 6].diff().fillna(0)
+        rain_df = month_df.iloc[:, 6].diff().fillna(0)
+        rain_df = rain_df[rain_df >= 0]
+        plot_rain(plt, pdf, rain_df, month_df, f'Niederschlag {month}', None, ['Niederschlag [mm]', 'Niederschlag [mm] (Regenmesser)'], 'b')
 
-        # Plot the Rain Gauge differences as a bar graph
-        ax6.bar(month_df.index, rain_diff, label='Niederschlag [mm]', color='b')
-        ax6.set_xlabel('Zeit')
-        ax6.set_ylabel('Niederschlag [mm]', color='b')
-        ax6.tick_params(axis='y', labelcolor='b')
-        ax6.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y %H:%M'))
-        ax6.xaxis.set_major_locator(mdates.AutoDateLocator())
+        plot_data(plt, pdf, month_df, [11], f'Warmwasser {month}', None, ['Temperatur [°C]'], ['orange'], 'tägl. Durchschnitt', ['k'])
 
-        #ax6.plot(month_df.index, month_df.iloc[:, 6], label='Täglicher Durchschnitt [°C]', color='b', linestyle='--', linewidth=2)
-
-        ax6.legend()
-        ax6.grid(True)
-
-        # Rotate x-axis labels for better readability
-        plt.setp(ax6.xaxis.get_majorticklabels(), rotation=45)
-
-        # Adjust layout to prevent clipping of tick-labels
-        plt.tight_layout()
-
-        # Set the title of the figure
-        fig.suptitle(f'Garten {month} - Regenmesser', fontsize=16)
-
-        # Save the current figure to the PDF
-        pdf.savefig(fig)
-
-        # Close the figure to free up memory
-        plt.close(fig)
