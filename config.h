1f0bfb46afd57869ae3b1b2c05e074875a4cac2a
Matthias Prinke <m.prinke@arcor.de>
2024-12-27 11:05:53 +0100

    Modified radio chip selection

--- config.h
+++ config.h
@@ -43,6 +43,7 @@
 // 20240710 Fixed pragma messages fro Firebeetle ESP32 pin config
 // 20240922 Bumped to RadioLib v7.0.0
 // 20240928 Modified for LoRaWAN v1.0.4 (requires no nwkKey)
+// 20241227 Modified radio chip selection
 //
 // ToDo:
 // - 
@@ -107,7 +108,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
     #pragma message("NOT TESTED!!!")
     #pragma message("ARDUINO_FEATHER_ESP32/ARDUINO_THINGPULSE_EPULSE_FEATHER defined; assuming RFM95W FeatherWing will be used")
     #pragma message("Required wiring: A to RST, B to DIO1, D to DIO0, E to CS")
-    #define LORA_CHIP SX1276
+    #define USE_SX1276
 
 #elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
     // Use pinning for Adafruit Feather ESP32S2 with RFM95W "FeatherWing" ADA3232
@@ -119,7 +120,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
     #pragma message("NOT TESTED!!!")
     #pragma message("ARDUINO_ADAFRUIT_FEATHER_ESP32S2 defined; assuming RFM95W FeatherWing will be used")
     #pragma message("Required wiring: A to RST, B to DIO1, D to DIO0, E to CS")
-    #define LORA_CHIP SX1276
+    #define USE_SX1276
 
 #elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32_V2)
     #define PIN_LORA_NSS      14
@@ -130,7 +131,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
     #pragma message("NOT TESTED!!!")
     #pragma message("ARDUINO_ADAFRUIT_FEATHER_ESP32_V2 defined; assuming RFM95W FeatherWing will be used")
     #pragma message("Required wiring: A to RST, B to DIO1, D to DIO0, E to CS")
-    #define LORA_CHIP SX1276
+    #define USE_SX1276
 
 #elif defined(ARDUINO_ESP32S3_POWERFEATHER)
     #define PIN_LORA_NSS      15
@@ -140,7 +141,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
     #define PIN_LORA_DIO2     RADIOLIB_NC
     #pragma message("ARDUINO_ESP32S3_POWERFEATHER defined; assuming RFM95W FeatherWing will be used")
     #pragma message("Required wiring: A to RST, B to DIO1, D to DIO0, E to CS")
-    #define LORA_CHIP SX1276
+    #define USE_SX1276
 
 #elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040)
   // Use pinning for Adafruit Feather RP2040 with RFM95W "FeatherWing" ADA3232
@@ -150,7 +151,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO    10
   #pragma message("ARDUINO_ADAFRUIT_FEATHER_RP2040 defined; assuming RFM95W FeatherWing will be used")
   #pragma message("Required wiring: A to RST, B to DIO1, D to DIO0, E to CS")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 // LilyGo 
 #elif defined(ARDUINO_TTGO_LORA32_V1) || defined(ARDUINO_TTGO_LoRa32_V1)
@@ -164,7 +165,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO     33
   #define PIN_LORA_DIO2     RADIOLIB_NC
   #pragma message ("TTGO LoRa32 v1 - no Display")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 #elif defined(ARDUINO_TTGO_LORA32_V2) || defined(ARDUINO_TTGO_LoRa32_V2)
   // https://github.com/espressif/arduino-esp32/blob/master/variants/ttgo-lora32-v2/pins_arduino.h
@@ -173,7 +174,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_IRQ      LORA_IRQ
   #define PIN_LORA_GPIO     RADIOLIB_NC
   #define PIN_LORA_DIO2     RADIOLIB_NC
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
   #pragma message ("TTGO_LoRa32_V2 + Display")
 
 #elif defined(ARDUINO_TTGO_LoRa32_v21new)
@@ -184,26 +185,26 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO     LORA_D1
   #define PIN_LORA_DIO2     RADIOLIB_NC
   #pragma message ("Using TTGO LoRa32 v2.1 marked T3_V1.6.1 + Display")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 #elif defined(ARDUINO_TBEAM_USE_RADIO_SX1262)
   #pragma error ("ARDUINO_TBEAM_USE_RADIO_SX1262 awaiting pin map")
-  #define LORA_CHIP SX1262
+  #define USE_SX1262
 
 #elif defined(ARDUINO_TBEAM_USE_RADIO_SX1276)
   #pragma error ("ARDUINO_TBEAM_USE_RADIO_SX1276 awaiting pin map")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 // AZ-Delivery
 #elif defined(ARDUINO_D1_MINI32)
-  // ESP32-WROOM-32 
+  // ESP32-WROOM-32
   #define PIN_LORA_NSS      27
   #define PIN_LORA_RST      32
   #define PIN_LORA_IRQ      21
   #define PIN_LORA_GPIO     33
   #define PIN_LORA_DIO2     RADIOLIB_NC
   #pragma message("wemos_d1_mini32 - WEMOS D1 MINI ESP32 defined; assuming RFM95W will be used")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 // Heltec
 #elif defined(ARDUINO_HELTEC_WIFI_LORA_32)
@@ -217,7 +218,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO     DIO1
   #define PIN_LORA_DIO2     DIO2
   #pragma message("ARDUINO_HELTEC_WIFI_LORA_32_V2")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 #elif defined(ARDUINO_HELTEC_WIFI_LORA_32_V3) || defined(ARDUINO_heltec_wifi_lora_32_V3)
   // Use pinning for Heltec WiFi LoRa32 V3
@@ -226,7 +227,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_IRQ   DIO0
   #define PIN_LORA_GPIO  BUSY_LoRa
   #pragma message("ARDUINO_HELTEC_WIFI_LORA_32_V3")
-  #define LORA_CHIP SX1262
+  #define USE_SX1262
 
 #elif defined(ARDUINO_HELTEC_WIRELESS_STICK)
   // https://github.com/espressif/arduino-esp32/blob/master/variants/heltec_wireless_stick/pins_arduino.h
@@ -236,33 +237,33 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO     DIO1
   #define PIN_LORA_DIO2     DIO2
   #pragma message("ARDUINO_HELTEC_WIRELESS_STICK")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 #elif defined(ARDUINO_HELTEC_WIRELESS_STICK_V3)
-  https://github.com/espressif/arduino-esp32/tree/master/variants/heltec_wireless_stick_v3
+  // https://github.com/espressif/arduino-esp32/tree/master/variants/heltec_wireless_stick_v3
   #define PIN_LORA_NSS      SS
   #define PIN_LORA_RST      RST_LoRa
   #define PIN_LORA_IRQ      DIO0
   #define PIN_LORA_GPIO     DIO1
   #define PIN_LORA_DIO2     DIO2
   #pragma message("ARDUINO_HELTEC_WIRELESS_STICK_V3")
-  #define LORA_CHIP SX1262
+  #define USE_SX1262
 
 // #elif defined(ARDUINO_heltec_wifi_kit_32_V2)
 //  Presumably no LoRa chip
 //  #pragma message ("ARDUINO_heltec_wifi_kit_32_V2 awaiting pin map")
-//  #define LORA_CHIP USE_SX1276
+//  #define USE_SX1276
 //  SX1276 radio = new Module(18, 26, 14, 35);
 
 // #elif defined(ARDUINO_heltec_wifi_kit_32_V3)
 //  Presumably no LoRa chip
 //   #pragma message ("Using Heltec WiFi LoRa32 v3 - Display + USB-C")
-//   #define LORA_CHIP USE_SX1262
+//   #define USE_SX1262
 //   SX1262 radio = new Module(8, 14, 12, 13);
 
 #elif defined(ARDUINO_CUBECELL_BOARD)
   #pragma error ("ARDUINO_CUBECELL_BOARD awaiting pin map")
-  #define LORA_CHIP SX1262
+  #define USE_SX1262
   //SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
 
 #elif defined(ARDUINO_CUBECELL_BOARD_V2)
@@ -278,7 +279,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
   #define PIN_LORA_GPIO     RADIOLIB_NC
   //#define PIN_LORA_GPIO     35 // manual connection  - only required for LMIC
   #pragma message("ARDUINO_M5STACK_CORE2 defined; assuming M5Stack Module LoRa868 will be used")
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 
 #elif defined(ARDUINO_DFROBOT_FIREBEETLE_ESP32)
@@ -304,7 +305,7 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
     #pragma message("Either LORAWAN_NODE or DFROBOT_COVER_LORA must be defined")
   #endif
 
-  #define LORA_CHIP SX1276
+  #define USE_SX1276
 
 #else
   #pragma message ("Unknown board - no automagic pinmap available")
@@ -326,7 +327,15 @@ const uint8_t subBand = 0;  // For US915, change this to 2, otherwise leave on 0
 
 #endif
 
-LORA_CHIP radio = new Module(PIN_LORA_NSS, PIN_LORA_IRQ, PIN_LORA_RST, PIN_LORA_GPIO);
+#if defined(USE_SX1276)
+  #define LORA_CHIP SX1276
+#elif defined(USE_SX1262)
+  #define LORA_CHIP SX1262
+#elif defined(USE_LR1121)
+  #define LORA_CHIP LR1121
+#else
+  #pragma message ("No radio chip selected")
+#endif
 
 // Copy over the EUI's & keys in to the something that will not compile if incorrectly formatted
 uint64_t joinEUI =   RADIOLIB_LORAWAN_JOIN_EUI;
@@ -339,7 +348,7 @@ uint8_t nwkKey[] = { 0 };
 #endif
 
 // Create the LoRaWAN node
-LoRaWANNode node(&radio, &Region, subBand);
+//LoRaWANNode node(&radio, &Region, subBand);
 
 // result code to text ...
 String stateDecode(const int16_t result) {
