ee8da041177a52fe2b5d9f95eaaf303247281d6e
Matthias Prinke <m.prinke@arcor.de>
2025-01-21 07:56:42 +0100

    Changed pBLEScan (file static) to _pBLEScan (member)

--- src/BleSensors/BleSensors.cpp
+++ src/BleSensors/BleSensors.cpp
@@ -47,13 +47,12 @@
 
 #include "BleSensors.h"
 
-static NimBLEScan *pBLEScan;
-
 class ScanCallbacks : public NimBLEScanCallbacks
 {
 public:
   std::vector<std::string> m_knownBLEAddresses; /// MAC addresses of known sensors
   std::vector<ble_sensors_t> *m_sensorData;     /// Sensor data
+  NimBLEScan *m_pBLEScan;
 
 private:
   int m_devices_found = 0; /// Number of known devices found
@@ -138,7 +137,7 @@ private:
     if (m_devices_found == m_knownBLEAddresses.size())
     {
       log_i("All devices found.");
-      pBLEScan->stop();
+      m_pBLEScan->stop();
     }
   }
 
@@ -150,7 +149,7 @@ private:
 
 void BleSensors::clearScanResults(void)
 {
-  pBLEScan->clearResults();
+  _pBLEScan->clearResults();
 }
 
 // Set all array members invalid
@@ -167,47 +166,21 @@ void BleSensors::resetData(void)
  */
 unsigned BleSensors::getData(uint32_t scanTime, bool activeScan)
 {
-  // From https://github.com/theengs/decoder/blob/development/examples/ESP32/ScanAndDecode/ScanAndDecode.ino:
-  // MyAdvertisedDeviceCallbacks are still triggered multiple times; this makes keeping track of received
-  // sensors difficult. Setting ScanFilterMode to CONFIG_BTDM_SCAN_DUPL_TYPE_DATA_DEVICE seems to
-  // restrict callback invocation to once per device as desired.
-  // NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DEVICE);
-  // NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DATA_DEVICE);
-  // NimBLEDevice::setScanDuplicateCacheSize(200);
-  // NimBLEDevice::init("");
-
-  //_pBLEScan = NimBLEDevice::getScan(); //create new scan
-
-  // Set the callback for when devices are discovered, no duplicates.
-  //_pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks(), false);
-  // MyAdvertisedDeviceCallbacks *myCb = new MyAdvertisedDeviceCallbacks();
-
-  // Copy some data required by the Callback
-  // myCb->m_pBLEScan = _pBLEScan;
-  // myCb->m_knownBLEAddresses = _known_sensors;
-  // myCb->m_sensorData = &data;
-
-  //_pBLEScan->setAdvertisedDeviceCallbacks(myCb);
-  //_pBLEScan->setActiveScan(activeScan); // Set active scanning, this will get more data from the advertiser.
-  //_pBLEScan->setInterval(97); // How often the scan occurs / switches channels; in milliseconds,
-  //_pBLEScan->setWindow(37);  // How long to scan during the interval; in milliseconds.
-  //_pBLEScan->setMaxResults(0); // do not store the scan results, use callback only.
-  //_pBLEScan->start(duration, false /* is_continue */);
-
-  // New
-  scanCallbacks.m_knownBLEAddresses = _known_sensors;
-  scanCallbacks.m_sensorData = &data;
   NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DATA_DEVICE);
   NimBLEDevice::setScanDuplicateCacheSize(200);
 
   NimBLEDevice::init("ble-scan");
-  pBLEScan = NimBLEDevice::getScan();
-  pBLEScan->setScanCallbacks(&scanCallbacks);
-  pBLEScan->setActiveScan(activeScan);
-  pBLEScan->setInterval(97);
-  pBLEScan->setWindow(37);
-  // pBLEScan->start(scanTime * 1000, false);
-  pBLEScan->getResults(scanTime * 1000, false);
+  _pBLEScan = NimBLEDevice::getScan();
+  _pBLEScan->setScanCallbacks(&scanCallbacks);
+  _pBLEScan->setActiveScan(activeScan);
+  _pBLEScan->setInterval(97);
+  _pBLEScan->setWindow(37);
+  scanCallbacks.m_knownBLEAddresses = _known_sensors;
+  scanCallbacks.m_sensorData = &data;
+  scanCallbacks.m_pBLEScan = _pBLEScan;
+  // Start scanning
+  // Blocks until all known devices are found or scanTime is expired
+  _pBLEScan->getResults(scanTime * 1000, false);
 
   return 0;
 }
