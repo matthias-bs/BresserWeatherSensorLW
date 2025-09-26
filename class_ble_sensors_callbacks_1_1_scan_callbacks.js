3eaa5cf4668c4360ef8176a56ac4f18962ceaabd
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2025-09-26 09:54:18 +0000

    Deploying to gh-pages from @ matthias-bs/BresserWeatherSensorLW@00c14221118e4afc239ee3054d644b1f58938976 🚀

--- class_ble_sensors_callbacks_1_1_scan_callbacks.js
+++ class_ble_sensors_callbacks_1_1_scan_callbacks.js
@@ -1,6 +1,8 @@
 var class_ble_sensors_callbacks_1_1_scan_callbacks =
 [
+    [ "m_devices_found", "class_ble_sensors_callbacks_1_1_scan_callbacks.html#aca0266b12eb71375469dc6bb01960aa3", null ],
     [ "m_knownBLEAddresses", "class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6679528e572ef6d75e877e5bcccb5470", null ],
     [ "m_pBLEScan", "class_ble_sensors_callbacks_1_1_scan_callbacks.html#aac4b4cd236064b196de2d66c9c29cacc", null ],
+    [ "m_rawIndexes", "class_ble_sensors_callbacks_1_1_scan_callbacks.html#a467e5b4775ecd00e785e01cc017a8830", null ],
     [ "m_sensorData", "class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6fc2cacf6fb18c6d6be45dbdb163b054", null ]
 ];
--- search/variables_6.js
+++ search/variables_6.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['m_5fknownbleaddresses_0',['m_knownBLEAddresses',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6679528e572ef6d75e877e5bcccb5470',1,'BleSensorsCallbacks::ScanCallbacks']]],
-  ['m_5fpblescan_1',['m_pBLEScan',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#aac4b4cd236064b196de2d66c9c29cacc',1,'BleSensorsCallbacks::ScanCallbacks']]],
-  ['m_5fsensordata_2',['m_sensorData',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6fc2cacf6fb18c6d6be45dbdb163b054',1,'BleSensorsCallbacks::ScanCallbacks']]]
+  ['m_5fdevices_5ffound_0',['m_devices_found',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#aca0266b12eb71375469dc6bb01960aa3',1,'BleSensorsCallbacks::ScanCallbacks']]],
+  ['m_5fknownbleaddresses_1',['m_knownBLEAddresses',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6679528e572ef6d75e877e5bcccb5470',1,'BleSensorsCallbacks::ScanCallbacks']]],
+  ['m_5fpblescan_2',['m_pBLEScan',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#aac4b4cd236064b196de2d66c9c29cacc',1,'BleSensorsCallbacks::ScanCallbacks']]],
+  ['m_5frawindexes_3',['m_rawIndexes',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#a467e5b4775ecd00e785e01cc017a8830',1,'BleSensorsCallbacks::ScanCallbacks']]],
+  ['m_5fsensordata_4',['m_sensorData',['../class_ble_sensors_callbacks_1_1_scan_callbacks.html#a6fc2cacf6fb18c6d6be45dbdb163b054',1,'BleSensorsCallbacks::ScanCallbacks']]]
 ];
